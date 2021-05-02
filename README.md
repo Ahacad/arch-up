ced6edeb22132974b6d7eff02267378fedcc25db
ahacad <ahacad@126.com>
2021-05-02 13:57:56 +0800

    feat: add new useme.sh

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+
+### 
+
+Using Arch Linux is a enjoyable experience, for you can customize it to your needs, installing Arch is not.
+
+This repo is an **opinioned** repo for installing basic and advanced tools **after you successfully install Arch and boot into it**.
+
+
--- using-centos.sh
+++ using-centos.sh
@@ -1,5 +1,14 @@
-yum install -y git tmux zsh snapd
+# install essential unix tools
 
+yum install -y git tmux zsh 
+## p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc #may face zsh verson problem on centos, and you need to install zsh manually for the right version, see here: https://gist.github.com/Semo/378fba2516a31f2608f0ad0161a73ab7
+
+yum install snapd
+
+# development tools
+yum groupinstall "Development tools"
 
 # install docker
 yum install -y yum-utils
@@ -11,3 +20,6 @@ yum -y install docker-ce docker-ce-cli containerd.io
 # install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
